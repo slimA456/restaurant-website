32886e09c380cf4e5ceba616b51100001ac7f42b
slimA4 <abubakarrsamura105@gmail.com>
2025-12-30 01:01:22 +0300

    first commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-content">
             <h2>order your favourite here</h2>
-            <p>choose from a diverse menu featuring a delectable array of dishes crsfted with the finest ingredient and culinary exoertise, our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
+            <p>choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredient and culinary exoertise, our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
             <button> view menu</button>
 
         </div>
